c7d8d4d6b3a167604c2990d6cff570ddd3e5bb28
zrlu <zrlu@uwaterloo.ca>
2020-04-14 17:36:46 +0000

    update rules

--- aggregation.py
+++ aggregation.py
@@ -1,7 +1,13 @@
 from wikitable import WikiPage
-
-def select(column, agg, table):
-    pass
+import pandas as pd
+from glob import glob
+import pdb
 
 if __name__ == '__main__':
-    pass
+    dataframes = []
+    csv_path_list = glob('wiki/*/*.csv')
+    for csv in csv_path_list:
+        print(csv)
+        df = pd.read_csv(csv)
+        dataframes.append(df)
+    pdb.set_trace()
