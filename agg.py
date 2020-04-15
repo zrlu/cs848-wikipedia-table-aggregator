e340a561acd704fc38374ec22d0a05495eaaa6f7
zrlu <zrlu@uwaterloo.ca>
2020-04-15 15:57:30 +0000

    update shell

--- agg.py
+++ agg.py
@@ -7,12 +7,14 @@ from rules import column_mapper
 def load(path='wiki/'):
     global cat
     dataframes = []
-    csv_path_list = glob(path + '/*/*.csv')
+    csv_path_list = glob(path + '/**/*.csv', recursive=True)
     for csv in csv_path_list:
         df = pd.read_csv(csv)
         df.rename(columns=column_mapper, inplace=True)
         dataframes.append(df)
     cat = pd.concat(dataframes)
+    print('{} csv file(s) loaded.'.format(len(csv_path_list)))
+    return True
 
 def show_info():
     cat.info(verbose=True)
@@ -33,16 +35,21 @@ def agg(agg, col_name):
     return None
 
 def help():
+    print('COMMANDS:')
     print('q: exit')
-    print('show_info(): show the columns of the dataframe')
-    print('cat: access the concatenated dataframe of wikipedia tables')
-    print("load(path=\'wiki\\\'): load csv files into a concatenated dataframe")
     print('h: display this message')
+    print('VARIABLES:')
+    print('cat: access the concatenated dataframe of wikipedia tables')
+    print('PYTHON FUNCTIONS:')
+    print('show_info(): show the columns of the dataframe, run once when this programs starts')
+    print("load(path=\'wiki\\\'): load csv files into a concatenated dataframe, run once when this program starts")
 
 if __name__ == '__main__':
   
     load()
     show_info()
+
+    print('type \'h\' to display help message')
 
     while True:
         try:
@@ -61,6 +68,6 @@ if __name__ == '__main__':
             break
         except EOFError:
             break
-        except Exception:
-            print('invalid expression')
+        except Exception as e:
+            print(e)
             continue
