adc62a33904d8055cbc07b92397198d2fc953622
zrlu <zrlu@uwaterloo.ca>
2020-04-15 23:43:15 +0000

    update README

--- agg.py
+++ agg.py
@@ -3,15 +3,17 @@
 import pandas as pd
 from glob import glob
 import pdb
-from rules import column_mapper
+import rules
+from importlib import reload
 
 def load(path='wiki/'):
     global cat
+    reload(rules)
     dataframes = []
     csv_path_list = glob(path + '/**/*.csv', recursive=True)
     for csv in csv_path_list:
         df = pd.read_csv(csv)
-        df.rename(columns=column_mapper, inplace=True)
+        df.rename(columns=rules.column_mapper, inplace=True)
         dataframes.append(df)
     cat = pd.concat(dataframes)
     print('{} csv file(s) loaded.'.format(len(csv_path_list)))
@@ -19,6 +21,12 @@ def load(path='wiki/'):
 
 def show_info():
     cat.info(verbose=True)
+
+def peek(col_name, dropna=False):
+    if dropna:
+        print(cat[col_name].dropna())
+    else:
+        print(cat[col_name])
 
 def agg(agg, col_name):
     col = pd.to_numeric(cat[col_name], errors='coerce')
@@ -43,7 +51,9 @@ def help():
     print('\t', 'cat: access the concatenated dataframe of wikipedia tables')
     print('PYTHON FUNCTIONS:')
     print('\t', 'show_info(): show the column information of the concatenated dataframe, run once when this programs starts')
-    print('\t', "load(path): load csv files under path into a concatenated dataframe, run once when this program starts, default path is 'wiki'")
+    print('\t', "load(path): reload csv files under path into a concatenated dataframe and the column name mapping rules, run once when this program starts, default path is 'wiki'")
+    print('\t', "agg(type, column): aggregate the column. type is in ['max', 'min', 'count', 'sum', 'mean'] ")
+    print('\t', "peek(column, dropna=False): peek values in a column, ignore NaN by setting dropna=True")
 
 if __name__ == '__main__':
   
