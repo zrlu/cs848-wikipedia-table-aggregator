33ff7e64b2caeac61ba09a1371bb9a0ccfacbc02
zrlu <zeranlu@hotmail.com>
2020-04-19 12:32:02 -0400

    Update rules.py

--- rules.py
+++ rules.py
@@ -4,6 +4,6 @@ summary_row_keywords = "sum|average|total|turnout|majority|summary|career|all-st
 cell_remove_special_symbols = "\*|†|~|\u200d"
 cell_replace_special_symbols = [("\u2010|\u2011|\u2012|\u2013|\u2014|\u2015", '-'), ('\xa0', ' ')]
 column_mapper = {
-    'FG%': 'field_goals',
-    'Field Goals': 'field_goals'
+    'Games': 'games_played',
+    'GP': 'games_played'
 }
